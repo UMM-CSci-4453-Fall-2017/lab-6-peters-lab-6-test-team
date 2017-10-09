6b885b44f8051600fffae1950ef466ab85d4d033
nathan-beneke <benek020@morris.umn.edu>
2017-10-09 18:02:30 -0500

    Async. How does it work? We don't know but the program works now.

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -13,57 +13,84 @@ connection.connect(function(err){
 	}
 });
 
-var rows;
+//var rows;
 
 connection.query('SHOW DATABASES', function(err, results, fields){
 	if(err){
 		console.log("invalid query" + err);
 	} else {
-		console.log("hello world".indexOf("o"));
-		iterateDatabases(results);
+//		console.log(results);
+		results.forEach(showTables);
 	}
 });
 
-iterateDatabases = function(rows){
 
-for (i = 0; i < rows.length; i++){
-	var first = rows[i].indexOf("'");
-	var last = rows[i].lastIndexOf("'");
-	var database = rows[i].substring(first, last + 1);
+iterateDatabases = function(row, index){
+
+
+	var row;
+	var database = row.Database;
 	console.log(database);
+
 	connection.query("USE " + database + ";", function(err, results, fields){
-		if(err){
-			console.log("Issue switching databases: " + err);
+
+		if (err){
+			console.log("issue switching databases " + err);
 		} else {
-			console.log("Now using " + database);
+		
+//			console.log(results);
+			(showTables);
 		}
-	})
-	connection.query("SHOW TABLES;", function(err, results, fields){
-		if(err){
-			console.log("issue with tables query: " + err);
-		} else {
-			for(result in results){
-				describeTable(result.Table);
-			}
-		}
+//	connection.query("SHOW TABLES;", function(err, results, fields){
+//		if(err){
+//			console.log("issue with tables query: " + err);
+//		} else {
+//			console.log("At line 38, database = " + database);
+//			console.log(results);
+//			console.log(fields);
+		//	results.forEach(describeTable, database);
+			
+//		}
 	
+//	})}
 	})
 }
+
+showTables = function(database, index){
+	var db = database.Database;
+	connection.query("SHOW TABLES IN " + db + ";", function(err, results, fields){
+		if(err){
+			console.log("Issue showing tables " + err);
+		} else {
+			results.forEach(describeTable, db);
+		}
+	})
 }
 
-describeTable = function(table){
-
-	connection.query("DESCRIBE " + table + ";", function(err, results, fields){
+describeTable = function(table, index){
+//console.log("Got to line 58 ");
+//console.log(fields);
+	//for(j = 0; j < tables.length; j++){
+	var name = "Tables_in_" + this;
+	var tableName = table[name];
+//	console.log(table);
+//	console.log("Database = " + this + " table = " + tableName);
+	//var db = fields[0].db;
+	//console.log("Line 65 database = " + db + " table = " + table);
+	
+	connection.query("DESCRIBE " + this + "." + tableName + ";", function(err, results, fields){
 		if(err){
 			console.log("Issue describing table " + err);
 		} else {
+			console.log(tableName);
 			console.log(results);
 		}
 	})
 }
 
+//connection.close();
 
-
+console.log("All done");
 
 
 
